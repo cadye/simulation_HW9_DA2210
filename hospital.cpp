59c58270c8de83cf3ded14cb8aaec8c49c3687b0
carlenge <carl.engelhardt@gmail.com>
2021-11-28 04:05:47 +0100

    hospital simulation complete

--- hospital.cpp
+++ hospital.cpp
@@ -5,20 +5,35 @@
 #include <vector>
 #include <cmath>
 
+// 300 visitors per day. a time unit is a second.
+const int dayInSeconds = 24 * 60 * 60;
+const double arrivalProbability = (double) 300 / dayInSeconds;
+const int totalDays = 365 * 5;
 
-int simulateHospital() {
-
+int simulateHospitalDay() {
+    int totalVisitors = 0;
+    for (int i = 0; i < dayInSeconds; i++) {
+        double random = (double) rand() / RAND_MAX;
+        if (random < arrivalProbability) totalVisitors++;
+    }
+    return totalVisitors;
 }
 
 int main() {
     srand(time(NULL));  // initiate random seed
+    std::vector<int> visitorsEachDay;
 
-
+    for (int i = 0; i < totalDays; i++) {
+        int visitors = simulateHospitalDay();
+        visitorsEachDay.push_back(visitors);
+    }
     
     // write output to file
     std::ofstream file;
     file.open("hospitalOutput.txt");
-    file << "l" << std::endl;
+    for (int visitors : visitorsEachDay) {
+        file << visitors << std::endl;
+    }
     file.close();
 
     return 0;
