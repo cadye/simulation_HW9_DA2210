cc35302fd632b41cdea4ac5fbb0a056067673cfb
carlenge <carl.engelhardt@gmail.com>
2021-11-28 01:29:03 +0100

    write output to file. probably interpreted the output correctly

--- stPetersburgParadox.cpp
+++ stPetersburgParadox.cpp
@@ -1,46 +1,51 @@
 #include <iostream>      /* IO */
+#include <fstream>
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
 #include <vector>
 #include <cmath>
 
 const int maxPayoff = 10000000;
-const int n = 1000; // maximum number of games to simulate
+const int n = 1000000; // number of games to simulate
+
+int simulateGame() {
+    int coinFlips = 0;
+    while (true) {
+        int coinToss = rand() % 2;
+        // game is over if coinToss = 1, or if we have reached the maximum payoff.
+        if (coinToss == 1 || coinFlips == (int) log2(maxPayoff)) {
+            int gamePayOff = pow(2, coinFlips);
+            return gamePayOff;
+        }
+        else {
+            // coinToss = 0, continue game.
+            coinFlips++;
+        }
+    }
+}
 
 int main() {
     srand(time(NULL));  // initiate random seed
-    std::vector<int> currentValues;
+    std::vector<int> currentPayOffs;
     std::vector<int> expectedPayOffs;
 
+    int sum = 0;
     for (int i = 1; i <= n; i++) {
-        for (int j = 0; j < i; j++) {
-            // simulate a game
-            int coinFlips = 0;
-            while (true) {
-                int coinToss = rand() % 2;
-                if (coinToss == 1) {
-                    int gamePayOff = pow(2, coinFlips);
-                    currentValues.push_back(gamePayOff);
-                    break;
-                }
-                else {
-                    coinFlips++;
-                }
-            }
-        }
-        int sum = 0;
-        for (int value : currentValues) {
-            sum += value;
-        }
-        int thisExpectedPayOff = sum / currentValues.size();
+        // simulate a game
+        int gamePayOff = simulateGame();
+        // calculate expected payOff (average) for the i simulated games.
+        sum += gamePayOff;
+        int thisExpectedPayOff = sum / i;
         expectedPayOffs.push_back(thisExpectedPayOff);
-        currentValues.clear();
-    }
-
-    for (int expectedPayOff : expectedPayOffs) {
-        std::cout << expectedPayOff << std::endl;
     }
     
+    // write output to file
+    std::ofstream file;
+    file.open("stPetersburgOutput.txt");
+    for (int expectedPayoff : expectedPayOffs) {
+        file << expectedPayoff << std::endl;
+    }
+    file.close();
 
     return 0;
 }
